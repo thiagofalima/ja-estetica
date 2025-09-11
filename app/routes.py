4c6e81d766619697c597143bf546b42edcbe3ce9
thiagofalima <thiagofalima@hotmail.com>
2025-09-11 17:37:12 -0300

    feat(login): Add login route and initial structure

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ pages = Blueprint("pages", __name__, template_folder="templates")
 @pages.route("/")
 def home():
     return render_template("home.html", title="JA - Estética")
+
+
+@pages.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html", title="JA - Estética | Login")
