6b5085f04b0f3df3974960ff7ed9275e5ee21dd1
thiagofalima <thiagofalima@hotmail.com>
2025-09-11 20:52:17 -0300

    chore(security): Implement safe SECRET_KEY configuration
    Implement the SECRET_KEY configuration using environment variables stored in .env.
    Add .env.example to guide other developers on how to configure their own keys.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from app.routes import pages
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente
+load_dotenv()
 
 
 def create_app():
 
     app = Flask(__name__)
     app.register_blueprint(pages)
+
+    # Configurando Secret Key da aplicação
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
     return app
