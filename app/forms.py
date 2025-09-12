e66f3c5687a63f5e3f985f0233b95ef8fdf6530b
thiagofalima <thiagofalima@hotmail.com>
2025-09-12 17:19:42 -0300

    feat(appointment): Add registration appointment form.
    Implement validation logic for date and time on the route.
    Also the appointment.html in order to render the wtf form.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, EmailField, PasswordField, SubmitField
+from wtforms import (StringField,
+                      DateField,
+                      TimeField, 
+                      EmailField, 
+                      PasswordField, 
+                      SubmitField,
+                      SelectField)
 from flask_wtf.recaptcha import RecaptchaField
-from wtforms.validators import InputRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange
 
 # Criando formulário de cadastro
 
@@ -9,37 +15,37 @@ from wtforms.validators import InputRequired, Email, Length, EqualTo
 class RegisterForm(FlaskForm):
     name = StringField(
         "Nome Completo *",
-        validators=[InputRequired(message="O nome completo é obrigatório.")]
+        validators=[DataRequired(message="O nome completo é obrigatório.")]
     )
     birth_date = DateField(
         "Data de Nascimento*",
-        validators=[InputRequired(message="A data de nascimento é obrigatória.")],
+        validators=[DataRequired(message="A data de nascimento é obrigatória.")],
     )
 
     phone_number = StringField(
         "Celular com DDD *",
-        validators=[InputRequired(message="O celular é obrigatório."),
+        validators=[DataRequired(message="O celular é obrigatório."),
                     Length(min=11, message="O celular precisa ter 11 digitos.")]
     )
 
     email = EmailField(
         "Email *",
         validators=[
-            InputRequired(message="O email é obrigatório."),
+            DataRequired(message="O email é obrigatório."),
             Email(message="Email inválido."),
         ],
     )
     password = PasswordField(
         "Senha *",
         validators=[
-            InputRequired(message="A senha é obrigatória."),
+            DataRequired(message="A senha é obrigatória."),
             Length(min=8, message="A senha deve ter no mínimo 8 caracteres."),
         ],
     )
 
     confirm_password = PasswordField(
         "Confimar a Senha*",
-        validators=[InputRequired(message="É necessário confirmar a senha"), 
+        validators=[DataRequired(message="É necessário confirmar a senha"), 
                     EqualTo("password", message="As senhas devem ser iguais.")]
     )
 
@@ -50,11 +56,11 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
 
     email = EmailField(
-        "Email *", validators=[InputRequired(message="O email é obrigatório.")]
+        "Email *", validators=[DataRequired(message="O email é obrigatório.")]
     )
 
     password = PasswordField(
-        "Senha *", validators=[InputRequired(message="A senha é obrigatória.")]
+        "Senha *", validators=[DataRequired(message="A senha é obrigatória.")]
     )
 
     recaptcha = RecaptchaField()
@@ -62,3 +68,32 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Entrar")
 
 
+class AppointmentForm(FlaskForm):
+    
+    procedure_name = SelectField(
+        "Procedimento *",
+        validators=[DataRequired(message="Escolha o procedimento.")],
+        choices=[
+            "*Escolha*",
+            "Design de Sobrancelhas",
+            "Brow Lamination",
+            "Lash Lifting",
+            "Detox Corporal",
+            "Limpeza de Pele",
+            "Depilação a Laser"
+        ]
+    )
+
+    procedure_date = DateField(
+        "Data", format='%Y-%m-%d',
+        validators=[DataRequired(message="Escolha o dia do seu procedimento.")],
+    )
+
+    procedure_time = TimeField(
+        "Hora", format="%H:%M",
+        validators=[DataRequired(message="Escolha o horário do seu procedimento.")]
+    )
+
+    submit = SubmitField("Agendar")
+
+    
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from passlib.hash import pbkdf2_sha256
 from uuid import uuid4
-from datetime import datetime
-from app.forms import RegisterForm, LoginForm
-from app.models import Client
+from datetime import datetime, date, time
+from app.forms import RegisterForm, LoginForm, AppointmentForm
+import time as tm
+from app.models import Client, Appointment
 
 pages = Blueprint("pages", __name__, template_folder="templates")
 
@@ -43,3 +44,28 @@ def login():
         return redirect(url_for("pages.home"))
 
     return render_template("login.html", title="JA - Estética | Login", form=form)
+
+
+@pages.route("/appointment/", methods=["GET", "POST"])
+def appointment():
+
+    form = AppointmentForm()
+
+    if request.method == "POST" and form.validate_on_submit():
+        # procedure = Appointment()
+
+        if form.procedure_date.data < date.today():
+            flash("A data do procedimento não pode ser no passado. Por favor, escolha uma data futura.", "danger")
+            # Retorna o template sem validar, mostrando o alerta e mantendo o usuário na mesma página
+            return render_template('appointment.html', form=form)
+        
+        elif form.procedure_time.data < time(8, 0) or form.procedure_time.data > time(17, 0):            
+            flash("Nosso horário é das 08:00 às 17:00", "danger")
+            # Retorna o template sem validar, mostrando o alerta e mantendo o usuário na mesma página
+            return render_template('appointment.html', form=form)
+        else:
+            flash("Procedimento agendado com sucesso!", "success")
+            return redirect(url_for("pages.home"))
+
+    return render_template("appointment.html", title="JA - Estética | Agendamento", form=form)
+
